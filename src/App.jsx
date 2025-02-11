1d69f58146387aa81e27a62f59a07b98fb46468d
selva1011 <selvaganapathi1011.selva@gmail.com>
2025-02-11 20:05:45 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ const App = () => {
   const [click, setClick] = useState(true)
   return (
     <div className='h-screen w-screen bg-[radial-gradient(circle,rgba(163,240,255,0.96),rgba(0,149,157,0.98))]"'>
-      <h1 className='text-4xl mx-auto absolute z-10 bg-transparent hidden'>selva</h1>
+      {/* <h1 className='text-4xl mx-auto absolute z-10 bg-transparent hidden'>selva</h1> */}
       {/* <Canvas orthographic camera={{ zoom: 100, position: [0, 0, 5] }}>
       <Text
       position={[0, 0, 0]} // Centered in the scene
@@ -24,12 +24,12 @@ const App = () => {
     </Text>
       </Canvas> */}
       <div className='fixed z-2 h-16 p-auto w-full flex justify-center items-center bg-none text-black'>
-        <button className='text-2xl text-[#004963] font-semibold' onClick={() => setClick(!click)}>{click ? "Hide Me!!" : "Click me!!"}</button>
+        <button className='text-2xl text-[#004963] font-semibold' onClick={() => setClick(!click)}>{click ? "Don't CLick here!!" : "Why U ate My Donut!!" }</button>
       </div>
       {click ? <Donut /> : null}
 
      <div className='fixed bottom-1 mx-auto w-full'>
-     <Popper />
+     {/* <Popper /> */}
       <RowColour />
      </div>
 
--- src/components/RowColour.jsx
+++ src/components/RowColour.jsx
@@ -1,19 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { div } from 'three/tsl'
+import Popper from './Popper'
 
 const RowColour = () => {
+    const [show, SetShow] = useState(false);
 
     return (
-        <div className='flex px-2 gap-6 w-[200px] justify-center mx-auto'>
-            {/* <button className='cursor-pointer gap-1 flex flex-col justify-end items-center'>
+        <>
+            {show && <Popper />}
+            <div className='flex px-2 gap-6 w-[200px] justify-center mx-auto'>
+                {/* <button className='cursor-pointer gap-1 flex flex-col justify-end items-center'>
                 <img src="/sweet.png" alt="" width='41' />
                 <p className='text-white text-sm font-semibold'>BUN</p>
             </button> */}
-            <button className='cursor-pointer gap-2 flex flex-col justify-end items-center'>
-                <img src="/doughnut.png" alt="" width='33' />
-                <p className='text-white text-sm font-semibold'>CREAM</p>
-            </button>
+                <button className='cursor-pointer gap-2 flex flex-col justify-end items-center' onClick={() => SetShow(!show)}>
+                    <img src="/doughnut.png" alt="" width='33' />
+                    <p className='text-white text-sm font-semibold'>CREAM</p>
+                </button>
 
-        </div>
+            </div>
+        </>
     )
 }
 
